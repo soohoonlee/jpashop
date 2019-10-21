85b2121b7599c3897d9d8e4b86a9d8d0b188bdd8
soohoonlee <jjinimania@gmail.com>
2019-10-21 15:26:26 +0900

    주문 Repository 개발

---
+++ src/main/java/jpabook/jpashop/order/OrderRepository.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.order;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+  private final EntityManager em;
+
+  public void save(Order order) {
+    em.persist(order);
+  }
+
+  public Order findOne(Long id) {
+    return em.find(Order.class, id);
+  }
+
+  // public List<Order> findAll(OrderSearch orderSearch) {}
+}
