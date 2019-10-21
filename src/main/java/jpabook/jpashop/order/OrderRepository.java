856db801a70714e2e20bfab40547c78d30d69341
soohoonlee <jjinimania@gmail.com>
2019-10-21 18:02:03 +0900

    주문 검색 기능 개발

--- src/main/java/jpabook/jpashop/order/OrderRepository.java
+++ src/main/java/jpabook/jpashop/order/OrderRepository.java
@@ -1,9 +1,18 @@
 package jpabook.jpashop.order;
 
+import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 @Repository
 @RequiredArgsConstructor
@@ -19,5 +28,63 @@ public class OrderRepository {
     return em.find(Order.class, id);
   }
 
-  // public List<Order> findAll(OrderSearch orderSearch) {}
+  public List<Order> findAllByString(OrderSearch orderSearch) {
+    String jpql = "select o from Order o join o.member m";
+    boolean isFirstCondition = true;
+
+    if (orderSearch.getOrderStatus() != null) {
+      if (isFirstCondition) {
+        jpql += " where";
+        isFirstCondition = false;
+      } else {
+        jpql += " and";
+      }
+      jpql += " o.status = :status";
+    }
+
+    if (StringUtils.hasText(orderSearch.getMemberName())) {
+      if (isFirstCondition) {
+        jpql += " where";
+        isFirstCondition = false;
+      } else {
+        jpql += " and";
+      }
+      jpql += " m.name like :name";
+    }
+
+    TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+        .setMaxResults(1000);
+
+    if (orderSearch.getOrderStatus() != null) {
+      query = query.setParameter("status", orderSearch.getOrderStatus());
+    }
+    if (StringUtils.hasText(orderSearch.getMemberName())) {
+      query = query.setParameter("name", orderSearch.getMemberName());
+    }
+    return query.getResultList();
+  }
+
+  /**
+   * JPA Criteria
+   */
+  public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+    CriteriaBuilder cb = em.getCriteriaBuilder();
+    CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+    Root<Order> o = cq.from(Order.class);
+    Join<Object, Object> m = o.join("member", JoinType.INNER);
+
+    List<Predicate> criteria = new ArrayList<>();
+
+    if (orderSearch.getOrderStatus() != null) {
+      Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
+      criteria.add(status);
+    }
+    if (StringUtils.hasText(orderSearch.getMemberName())) {
+      Predicate name = cb.like(m.get("name"), "%" + orderSearch.getMemberName() + "%");
+      criteria.add(name);
+    }
+    cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+    TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
+    return query.getResultList();
+  }
 }
