547e4e0c8b4ea690b3c93dec4f67ed21ffce832e
soohoonlee <jjinimania@gmail.com>
2019-10-21 15:23:57 +0900

    주문, 주문상품 Entity 개발

--- src/main/java/jpabook/jpashop/order/Order.java
+++ src/main/java/jpabook/jpashop/order/Order.java
@@ -18,6 +18,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import jpabook.jpashop.delivery.Delivery;
+import jpabook.jpashop.delivery.DeliveryStatus;
 import jpabook.jpashop.member.Member;
 import lombok.Getter;
 import lombok.Setter;
@@ -62,5 +63,39 @@ public class Order {
   public void setDelivery(Delivery delivery) {
     this.delivery = delivery;
     delivery.setOrder(this);
+  }
+
+  public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+    Order order = new Order();
+    order.setMember(member);
+    order.setDelivery(delivery);
+    for (OrderItem orderItem : orderItems) {
+      order.addOrderItem(orderItem);
+    }
+    order.setStatus(OrderStatus.ORDER);
+    order.setOrderDate(LocalDateTime.now());
+    return order;
+  }
+
+  /**
+   * 주문 취소
+   */
+  public void cancel() {
+    if (delivery.getStatus() == DeliveryStatus.COMP) {
+      throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+    }
+    this.setStatus(OrderStatus.CANCEL);
+    for (OrderItem orderItem : orderItems) {
+      orderItem.cancel();
+    }
+  }
+
+  /**
+   * 전체 주문 가격 조회
+   */
+  public int getTotalPrice() {
+    return orderItems.stream()
+        .mapToInt(OrderItem::getTotalPrice)
+        .sum();
   }
 }
--- src/main/java/jpabook/jpashop/order/OrderItem.java
+++ src/main/java/jpabook/jpashop/order/OrderItem.java
@@ -33,4 +33,24 @@ public class OrderItem {
   private int orderPrice;
 
   private int count;
+
+  public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+    OrderItem orderItem = new OrderItem();
+    orderItem.setItem(item);
+    orderItem.setOrderPrice(orderPrice);
+    orderItem.setCount(count);
+    item.removeStock(count);
+    return orderItem;
+  }
+
+  public void cancel() {
+    getItem().addStock(count);
+  }
+
+  /**
+   * 주문상품 전체 가격 조회
+   */
+  public int getTotalPrice() {
+    return getOrderPrice() * getCount();
+  }
 }
