74bbb566ceebbf0c97cece913ca9789722b13607
soohoonlee <jjinimania@gmail.com>
2019-11-12 16:16:21 +0900

    주문 목록 검색, 취소

--- src/main/java/jpabook/jpashop/order/OrderController.java
+++ src/main/java/jpabook/jpashop/order/OrderController.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -36,6 +38,19 @@ public class OrderController {
       @RequestParam("itemId") Long itemId,
       @RequestParam("count") int count) {
     orderService.order(memberId, itemId, count);
+    return "redirect:/orders";
+  }
+
+  @GetMapping("/orders")
+  public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) {
+    List<Order> orders = orderService.findOrders(orderSearch);
+    model.addAttribute("orders", orders);
+    return "order/orderList";
+  }
+
+  @PostMapping("/orders/{orderId}/cancel")
+  public String cancelOrder(@PathVariable("orderId") Long orderId) {
+    orderService.cancelOrder(orderId);
     return "redirect:/orders";
   }
 }
