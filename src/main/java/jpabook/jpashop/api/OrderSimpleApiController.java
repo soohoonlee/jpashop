c4b487a046c33137e8f719ebd08e09cb0641eec6
soohoonlee <jjinimania@gmail.com>
2020-03-06 16:04:54 +0900

    간단한 주문 조회 V1: 엔티티를 직접 노출

---
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.api;
+
+import java.util.List;
+import jpabook.jpashop.order.Order;
+import jpabook.jpashop.order.OrderRepository;
+import jpabook.jpashop.order.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * xToOne(ManyToOne, OneToOne)
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+  private final OrderRepository orderRepository;
+
+  @GetMapping("/api/v1/simple-orders")
+  public List<Order> ordersV1() {
+    List<Order> all = orderRepository.findAllByString(new OrderSearch());
+    for (Order order : all) {
+      order.getMember().getName(); // Lazy 강제 초기화
+      order.getDelivery().getAddress(); // Lazy 강제 초기화
+    }
+    return all;
+  }
+}
--- src/main/java/jpabook/jpashop/member/Member.java
+++ src/main/java/jpabook/jpashop/member/Member.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.member;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Column;
@@ -26,6 +27,7 @@ public class Member {
   @Embedded
   private Address address;
 
+  @JsonIgnore
   @OneToMany(mappedBy = "member")
   private List<Order> orders = new ArrayList<>();
 }
